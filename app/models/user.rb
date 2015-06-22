3b8153fcea15927ae70687749e18661977626f01
Yhoon Kim <yhoonkim01@gmail.com>
2015-06-22 15:25:04 +0900

    fix: calcualte score

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,20 @@ class User < ActiveRecord::Base
 	def make_score
     user_result= self.vote_results
     result_a = []
-    user_result.each do |a_user_result|
-      this_results = VoteResult.where(vote_id: a_user_result.vote_id).where(user_id: nil).where(result: a_user_result.result)
-      Representative.all.each do |r|
-        result_a.push({representative: r, count: this_results.where(representative_id: r.id).count} )
+    Representative.all.each do |r|
+      count = 0
+      r.vote_results.each do |r_vote_result|
+        user_result.each do |a_user_result|
+          if r_vote_result.vote_id == a_user_result.vote_id && r_vote_result.result == a_user_result.result
+            count +=1
+          end
+        end
       end
+      result_a.push({representative: r, count: count})
     end
     result_a
 
-      
-  end  
+
+  end
 end
+
