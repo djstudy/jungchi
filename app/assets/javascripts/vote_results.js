a8dd5066d9523b13e94914e940e4fa0ee18617d6
Yhoon Kim <yhoonkim01@gmail.com>
2015-06-22 16:29:09 +0900

    feat: add carousel in report page

---
+++ app/assets/javascripts/vote_results.js
@@ -0,0 +1,9 @@
+$(document).on('ready page:load', function () {
+  $('.representatives-carousel').slick({
+    dots: true,
+    infinite: false,
+    arrows: true,
+    slidesToShow: 1,
+
+  });
+});
