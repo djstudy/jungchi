347fe769632f26f3f7224553332f138c7c0443fa
Yhoon Kim <yhoonkim01@gmail.com>
2015-06-30 11:11:39 +0900

    feat: format for vote_infos

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -1,4 +1,9 @@
 class Representative < ActiveRecord::Base
   has_many :vote_results
   has_many :vote_infos, foreign_key: :speaker_id
+
+  def profile_img_url
+    "http://watch.peoplepower21.org/images/member/#{id}.jpg"
+  end
+
 end
--- app/models/vote_info.rb
+++ app/models/vote_info.rb
@@ -1,7 +1,22 @@
 class VoteInfo < ActiveRecord::Base
+  INFO_TYPES = ["prev", "yes", "no"]
+  INFO_TYPES.each  do |type|
+    define_method("is_#{type}?") do
+      info_type == type
+    end
+  end
+
   belongs_to :vote
   belongs_to :representative, foreign_key: :speaker_id
+  scope :opinions, -> {
+    vote_infos = VoteInfo.arel_table
+    where(vote_infos[:info_type].not_eq("prev")).order(:sequence)
+  }
 
 
+
+  def self.previously
+    find_by_info_type("prev")
+  end
 
 end
