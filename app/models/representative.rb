763dbc40dccbf4f9dff05d0ccfa11d627f2e7090
“nahbee10git config --global user.email “nahbee10@naver.com <naheekim@Nahees-MacBook-Pro.local>
2015-07-06 21:15:12 +0900

    heroku deploy

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -2,7 +2,13 @@ class Representative < ActiveRecord::Base
   has_many :vote_results
   has_many :vote_infos, foreign_key: :speaker_id
 
+  def profile_result_img_url
+    ActionController::Base.helpers.asset_path( "#{id}.jpg") 
+    # "http://watch.peoplepower21.org/images/member/#{id}.jpg"
+  end
+
   def profile_img_url
+    # ActionController::Base.helpers.asset_path( "#{id}.jpg") 
     "http://watch.peoplepower21.org/images/member/#{id}.jpg"
   end
 
