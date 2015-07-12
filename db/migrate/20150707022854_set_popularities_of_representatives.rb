4ca8a7f19e813e1d74ecd24ae5e11cd2df464de6
Yhoon Kim <yhoonkim01@gmail.com>
2015-07-12 14:18:06 +0900

    set named list

--- db/migrate/20150707022854_set_popularities_of_representatives.rb
+++ db/migrate/20150707022854_set_popularities_of_representatives.rb
@@ -1,6 +1,6 @@
 class SetPopularitiesOfRepresentatives < ActiveRecord::Migration
   def up
-    named = ["장하나", "박지원", "문재인", "박영선", "안철수", "김한길", "우윤근", "문희상", "이정현", "유승민", "나경원", "최경환","이완구","서청원","김무성","주호영"]
+    named = ["박지원", "문재인", "안철수", "김한길",  "이정현", "유승민", "나경원", "최경환","이완구","김무성"]
     Representative.all.each do |r|
       if(named.include?(r.name))
         r.update(popularity: "high")
