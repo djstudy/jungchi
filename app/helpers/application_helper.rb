028c0498099cce1c6aae675291dcdfd29f37edee
Yhoon Kim <yhoonkim01@gmail.com>
2015-07-12 14:36:40 +0900

    fix: og_tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,14 +27,14 @@ module ApplicationHelper
     meta_image =""
     meta_title =""
     if reporting_results
-      meta_image = "<meta property=\"og:image\" content=\"#{reporting_results.first[:representative].profile_result_img_url}\" />" 
-      meta_title = "<meta property=\"og:title\" content=\"#{reporting_named_result[:representative].name} 보다는 #{reporting_results.first[:representative].name}\" />"       
+      meta_image = "<meta property=\"og:image\" content=\"#{request.protocol}#{request.host_with_port}#{reporting_results.first[:representative].profile_result_og_img_url}\" />"
+      meta_title = "<meta property=\"og:title\" content=\"#{reporting_named_result[:representative].name} 보다는 #{reporting_results.first[:representative].name}\" />"
     end
 
-    meta_site_name = "<meta property=\"og:site_name\" content=\"정친(政親)\" />" 
-    meta_url = "<meta property=\"og:url\" content=\"#{request.original_url}\" />" 
-    meta_description = "<meta property=\"og:description\" content=\"국회의원 300명 중에 당신의 정친(政親)은 누굴까요? 나경원? 이완구? 문재인? 안철수? 정친(政親)은 5분 안으로 300명의 국회의원 중 당신과 가장 잘 맞는 정치인을 찾아드립니다.\" />" 
-    meta_app_id = "<meta property=\"fb:app_id\" content=\"475973985913779\" />" 
+    meta_site_name = "<meta property=\"og:site_name\" content=\"정친(政親)\" />"
+    meta_url = "<meta property=\"og:url\" content=\"#{request.original_url}\" />"
+    meta_description = "<meta property=\"og:description\" content=\"국회의원 300명 중에 당신의 정친(政親)은 누굴까요? 나경원? 이완구? 문재인? 안철수? 정친(政親)은 5분 안으로 300명의 국회의원 중 당신과 가장 잘 맞는 정치인을 찾아드립니다.\" />"
+    meta_app_id = "<meta property=\"fb:app_id\" content=\"475973985913779\" />"
     meta_title + meta_site_name + meta_url + meta_image + meta_description + meta_app_id
   end
 end
--- app/models/representative.rb
+++ app/models/representative.rb
@@ -2,13 +2,21 @@ class Representative < ActiveRecord::Base
   has_many :vote_results
   has_many :vote_infos, foreign_key: :speaker_id
 
+  def profile_result_og_img_url
+
+
+      # result = "#{request.protocol}#{request.host_with_port}"
+      result = ActionController::Base.helpers.image_path( "#{id}.jpg")
+
+  end
+
   def profile_result_img_url
-    ActionController::Base.helpers.asset_path( "#{id}.jpg") 
+    ActionController::Base.helpers.asset_path( "#{id}.jpg")
     # "http://watch.peoplepower21.org/images/member/#{id}.jpg"
   end
 
   def profile_img_url
-    # ActionController::Base.helpers.asset_path( "#{id}.jpg") 
+    # ActionController::Base.helpers.asset_path( "#{id}.jpg")
     "http://watch.peoplepower21.org/images/member/#{id}.jpg"
   end
 
