c33927bc22053fef790ecd3968e96105f66bc51f
Junhee Woo <junhee.woo@gmail.com>
2015-08-29 01:26:59 +0900

    connect data and design

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -11,71 +11,60 @@ class ReportsController < ApplicationController
   		minus_time += a_time - b_time
   	end
 
-  	lap_time = minus_time / finisher.count
-  	report_users = [User.count, finisher.count,lap_time]
+    lap_time = 0
+    if finisher.count > 0
+      lap_time = minus_time / finisher.count
+    end
+  	@report_users = [User.count, finisher.count,lap_time]
   	#raise report_users.inspect
 
   	all_vote_result = VoteResult.all
-  	yes_table = Hash.new
-  	no_table = Hash.new
+  	@yes_table = Hash.new
+  	@no_table = Hash.new
 
-  	all_vote_result.each do |v|
-  		if yes_table[v.vote_id] == nil or no_table[v.vote_id] == nil
-  			yes_table[v.vote_id] = 0
-  			no_table[v.vote_id] = 0
-  		end
-  		if v.result == "chanseong"
-  			yes_table[v.vote_id] = yes_table[v.vote_id] + 1
-  		elsif v.result == "bandae"
-  			no_table[v.vote_id] = no_table[v.vote_id] + 1
-  		end
-  	end
+    Vote.all.each do |v|
+      aggr = v.vote_results.where(representative_id: nil).group(:result).count
+      @yes_table[v.id] = aggr["chanseong"] || 0
+      @no_table[v.id] = aggr["bandae"] || 0
+    end
+  	
 
   	reps = Representative.all
   	reps_score = Hash.new
-
+    @vote_id_arr = Vote.pluck(:id).uniq
   	reps.each do |r|
   		r_vote_results = r.vote_results
   		r_vote_results.each do |r_vote|
   			if r_vote.result == "chanseong"
-  				reps_score[r.id] = yes_table[r_vote.vote_id]
-  				reps_score[r.id] = reps_score[r.id] - no_table[r_vote.vote_id]
+  				reps_score[r.id] = @yes_table[r_vote.vote_id]
+  				reps_score[r.id] = reps_score[r.id] - @no_table[r_vote.vote_id]
   			elsif r_vote.result == "bandae"
-  				reps_score[r.id] = no_table[r_vote.vote_id]
-  				reps_score[r.id] = reps_score[r.id] - yes_table[r_vote.vote_id]
+  				reps_score[r.id] = @no_table[r_vote.vote_id]
+  				reps_score[r.id] = reps_score[r.id] - @yes_table[r_vote.vote_id]
   			end
   		end
   	end
-  	ddd =reps_score.sort_by {|_key, value| value}
-  	#raise reps_score[0].inspect
-  	puts reps_score[1]
-  	puts reps_score[2]
+  	top_reps =reps_score.sort_by {|_key, value| value}
+  	@first  = reps.select{ |r| r.id = top_reps[0][0]}
+    @second  = reps.select{ |r| r.id = top_reps[1][0]}
+    @third  = reps.select{ |r| r.id = top_reps[2][0]}
 
   	final_scores = []
-  	ddd.each do |d|
+  	top_reps.each do |d|
   		final_scores.push(d[1])
   	end
   	
 
-  	reps_results = all_vote_result.select { |r| r.representative_id != nil }
-  	reps_yes_table = Hash.new
-  	reps_no_table = Hash.new
+  	@reps_yes_table = Hash.new
+  	@reps_no_table = Hash.new
 
-  	reps_results.each do |v|
+    Vote.all.each do |v|
+      aggr = v.vote_results.where(user_id: nil).group(:result).count
+      @reps_yes_table[v.id] = aggr["chanseong"]
+      @reps_no_table[v.id] = aggr["bandae"] + aggr["gigwon"]
+    end
+    
 
-  		if reps_yes_table[v.vote_id] == nil or reps_no_table[v.vote_id] == nil
-  			reps_yes_table[v.vote_id] = 0
-  			reps_no_table[v.vote_id] = 0
-  		end
-
-  		if v.result == "chanseong"
-  			reps_yes_table[v.vote_id] = reps_yes_table[v.vote_id] + 1
-  		elsif v.result == "bandae" or v.result == "gyeolseok" or v.result == "cheongga" or v.result == "boolcham" or v.result == "gigwon"
-  			reps_no_table[v.vote_id] = reps_no_table[v.vote_id] + 1
-  		end
-
-  	end
-  		raise final_scores.standard_deviation.inspect
   end
 
 
--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,15 @@
 module ReportsHelper
+	def score_calc(yes_arr, no_arr, result_id)
+		number_no   = no_arr[result_id]
+		number_yes = yes_arr[result_id]
+
+		if number_no + number_yes > 0
+			return (number_yes / (number_no + number_yes).to_f)*100
+		else
+			return 0
+		end
+
+
+
+	end
 end
