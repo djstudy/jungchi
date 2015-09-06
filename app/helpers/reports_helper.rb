a7a7c204a9eb92f20c3ab06f7102161749217c4b
Yhoon Kim <yhoonkim01@gmail.com>
2015-09-07 00:03:16 +0900

    flex problem?

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -4,7 +4,7 @@ module ReportsHelper
 		number_yes = yes_arr[result_id]
 
 		if number_no + number_yes > 0
-			return (number_yes / (number_no + number_yes).to_f)*100
+			return ((number_yes / (number_no + number_yes).to_f) * 100 ).round(2)
 		else
 			return 0
 		end
