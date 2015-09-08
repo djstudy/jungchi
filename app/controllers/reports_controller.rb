5c299f6bd828d6797efc07d1d92aa93fa2bab9a7
Junhee Woo <junhee.woo@gmail.com>
2015-09-08 11:48:19 +0900

    convert score and change explanation
    - convert score into "6 scale"
    - change explanation as the way to get score has been changed

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,25 +2,25 @@ require 'descriptive_statistics'
 
 class ReportsController < ApplicationController
   def index
-  	finisher = User.all.select { |u| u.vote_results.count >= 6  && u.vote_results.find_by_vote_id(9041) != nil}
-  	minus_time = 0
-  	finisher.each do |t|
-  		a_time = t.vote_results.find_by_vote_id(9041).created_at
-  		b_time = t.created_at
+    finisher = User.all.select { |u| u.vote_results.count >= 6  && u.vote_results.find_by_vote_id(9041) != nil}
+    minus_time = 0
+    finisher.each do |t|
+      a_time = t.vote_results.find_by_vote_id(9041).created_at
+      b_time = t.created_at
 
-  		minus_time += a_time - b_time
-  	end
+      minus_time += a_time - b_time
+    end
 
     lap_time = 0
     if finisher.count > 0
       lap_time = minus_time / finisher.count
     end
-  	@report_users = [User.count, finisher.count,lap_time]
-  	#raise report_users.inspect
+    @report_users = [User.count, finisher.count,lap_time]
+    #raise report_users.inspect
 
-  	all_vote_result = VoteResult.all
-  	@yes_table = Hash.new
-  	@no_table = Hash.new
+    all_vote_result = VoteResult.all
+    @yes_table = Hash.new
+    @no_table = Hash.new
 
     Vote.all.each do |v|
       aggr = v.vote_results.where(representative_id: nil).group(:result).count
@@ -29,34 +29,38 @@ class ReportsController < ApplicationController
     end
 
 
-  	reps = Representative.all
-  	reps_score = Hash.new
+    reps = Representative.all
+    reps_score = Hash.new
     @votes = Vote.order(:id)
-  	reps.each do |r|
+    reps.each do |r|
       reps_score[r.id] = 0
-  		r_vote_results = r.vote_results
-  		r_vote_results.each do |r_vote|
-  			if r_vote.result == "chanseong"
-  				reps_score[r.id] += @yes_table[r_vote.vote_id]
-  			elsif r_vote.result == "bandae" || r_vote.result == "gigwon"
-  				reps_score[r.id] += @no_table[r_vote.vote_id]
+      r_vote_results = r.vote_results
+      r_vote_results.each do |r_vote|
+      counter = @yes_table[r_vote.vote_id] + @no_table[r_vote.vote_id]
+      if counter == 0
+        counter = 1
+      end
+        if r_vote.result == "chanseong"
+          reps_score[r.id] += @yes_table[r_vote.vote_id] / counter
+        elsif r_vote.result == "bandae" || r_vote.result == "gigwon"
+          reps_score[r.id] += @no_table[r_vote.vote_id] / counter
         else
           reps_score[r.id] += ( @no_table[r_vote.vote_id] + @yes_table[r_vote.vote_id] ) * 0.5
-  			end
-  		end
-  	end
-  	top_reps =reps_score.sort_by {|_key, value| -value}
-  	@top3_with_score = top_reps[0..2].map{ |rep| {rep: Representative.find(rep[0]), score: rep[1]} }
+        end
+      end
+    end
+    top_reps =reps_score.sort_by {|_key, value| -value}
+    @top3_with_score = top_reps[0..2].map{ |rep| {rep: Representative.find(rep[0]), score: rep[1]} }
     @last_with_score = {rep: Representative.find(top_reps.last[0]), score: top_reps.last[1] }
 
     score_data = top_reps.map{ |rep| rep[1]}
     @score_mean = score_data.mean
     @score_std = score_data.standard_deviation
 
-  	final_scores = []
-  	top_reps.each do |d|
-  		final_scores.push(d[1])
-  	end
+    final_scores = []
+    top_reps.each do |d|
+      final_scores.push(d[1])
+    end
 
     party_scores = {}
     reps_score.each do |rep_score|
@@ -69,8 +73,8 @@ class ReportsController < ApplicationController
     @parties = @parties.sort_by! {|e| -e[:avg_score]}
 
 
-  	@reps_yes_table = Hash.new
-  	@reps_no_table = Hash.new
+    @reps_yes_table = Hash.new
+    @reps_no_table = Hash.new
 
     Vote.all.each do |v|
       aggr = v.vote_results.where(user_id: nil).group(:result).count
